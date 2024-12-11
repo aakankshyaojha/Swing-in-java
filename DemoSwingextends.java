e4f3f466626a4f78c7accc1886a1142c7fb947c3
Aakankshya Ojha <aakankshyaojha123@gmail.com>
2024-12-11 12:04:45 +0545

    Add files via upload

---
+++ DemoSwingextends.java
@@ -0,0 +1,22 @@
+package com.swing;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class DemoSwingextends extends JFrame {
+	DemoSwingextends() {
+		JButton b = new JButton("click");
+		b.setBounds(130, 100, 100, 40);
+		add(b);
+		setSize(400, 500);
+		setLayout(null);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new DemoSwingextends();
+
+	}
+
+}
